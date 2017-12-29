bc69145ebffe841d764f3c871ea38a777304ec56
DarcyOlson <darcy.olson94@gmail.com>
2017-12-28 17:39:17 -0800

    Main file for the cognitive games

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+									***INTRODUCTION***
+
+This is a piece of code to make a nice interface for all the other code I have written for
+the cognitive games code. Its goal is to give the user a choice bar to select different
+games. Previously, users had no option to play both of numeric memory and math at the same
+time. Now, they can play both using the interface designed below.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "/Users/darcy/Desktop/Programming/C/cognitive-games/Numeric Memory/numerical-memory-game.c"
+#include "welcome-messages.h"
+
+int main(){
+	opening_message();
+	return 0;	
+}
